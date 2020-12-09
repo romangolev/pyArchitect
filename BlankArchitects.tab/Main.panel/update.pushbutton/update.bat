787f71e72c7e5c261146216eab6d6d25fcb314ff
romangolev <rv.golev@gmail.com>
2020-12-10 00:02:46 +0300

    update 0.1.6
    update button for bundle fixed

---
+++ BlankArchitects.tab/Main.panel/update.pushbutton/update.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+pyrevit extensions delete BlankArchitects --debug
+pyrevit extend ui BlankArchitects https://github.com/BlankArchitects/pyBlank.git --debug
+@ECHO ----------------------------------------------------------
+@ECHO -----------------RESTART--PYREVIT-------------------------
+@ECHO ----------------------------------------------------------
+@PAUSE
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pyBlank
+![blank logo](https://raw.githubusercontent.com/BlankArchitects/pyBlank/main/docs/static/blank.jpg)
+## pyBlank 
+an Architectural and Interiors design extension
