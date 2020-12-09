a18a2437ccdddcd1758f6bff2d4a9b0aefd89382
romangolev <rv.golev@gmail.com>
2020-12-09 22:37:01 +0300

    update button edit

--- BlankArchitects.tab/Main.panel/update.pushbutton/Update_script.py
+++ BlankArchitects.tab/Main.panel/update.pushbutton/Update_script.py
@@ -1,28 +1,52 @@
+# -*- coding: utf-8 -*-
+# pylint: skip-file
+# by Roman Golev 
+# Blank Architects
+
 __context__ = 'zero-doc'
 
 import os
 #from rpm.system.ui import UI
-from pyrevit.coreutils.ribbon import ICON_LARGE
+#from pyrevit.coreutils.ribbon import ICON_LARGE
 from pyrevit import versionmgr
 import blank
 from pyrevit.versionmgr import updater
+from pyrevit.coreutils import git
+from pyrevit import forms
+import urllib2
 
-"""
-__context__ = 'zero-doc'
+from pyrevit.coreutils import ribbon
+from pyrevit.versionmgr import updater
+from pyrevit.userconfig import user_config
+from pyrevit import script
+
+# Get installed version
+blankinstalledversion = blank.get_version()
+
+# Get versiob inside the github repository
+data = urllib2.urlopen('https://raw.githubusercontent.com/BlankArchitects/pyBlank/main/lib/blank/version')
+for line in data: 
+	blankgitversion = line
+
 
 def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
-	if os.system('git --version') != 0:
-		return False
-	if UI.checkUpdates():
-		ui_button_cmp.set_title('Install\nUpdates')
-		update_icon = script_cmp.get_bundle_file('icon-has-updates.png')
-		ui_button_cmp.set_icon(update_icon, icon_size=ICON_LARGE)
-	return True
+	try:
+		has_update_icon = script_cmp.get_bundle_file('icon-hasupdates.png')
+		if blankgitversion != blankinstalledversion :
+			ui_button_cmp.set_icon(has_update_icon,
+                                   icon_size=ribbon.ICON_LARGE)
+	except:
+		0
 
-if __name__ == '__main__':
-	UI.checkUpdates(True)
-"""
-print(blank.get_version()) 
-print(os.system('git --version'))
-print(versionmgr.get_pyrevit_cli_version())
-print(updater.get_thirdparty_ext_repos())
+
+
+
+if blankgitversion == blankinstalledversion :
+	notification = forms.alert(  'Установлена последняя версия расширения\n'
+								'BlankArchitects for Revit\n',
+								ok=False, yes=True, no=True)
+	print True
+
+else:
+	#updater.repo()
+	print False
---
+++ BlankArchitects.tab/Main.panel/update.pushbutton/test.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+@------------------------
+@PAUSE
+@TEST
+@PAUSE
