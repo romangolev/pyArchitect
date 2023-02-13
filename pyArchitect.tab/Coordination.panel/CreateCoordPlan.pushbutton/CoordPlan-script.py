6e20ba8fb02ec72df7083f247a8c503ad7c609c8
Roman Golev <67314258+romangolev@users.noreply.github.com>
2023-02-13 05:56:24 +0100

    Update CoordPlan-script.py
    Coord plan fully new logic. Now it does create a new ViewType and finds if it exists in a project.

--- pyArchitect.tab/Coordination.panel/CreateCoordPlan.pushbutton/CoordPlan-script.py
+++ pyArchitect.tab/Coordination.panel/CreateCoordPlan.pushbutton/CoordPlan-script.py
@@ -20,6 +20,10 @@ Reveals Base Point in active view
 Shift+Click\
 Показывает базовую точку на выбранном виде
 """
+
+#TODO: Add lang driven dialogs
+#HOST_APP.language == LanguageType.English_USA:
+
 __author__ = 'Roman Golev'
 __title__ = "Coord\nPlan"
 #__helpurl__ = ""
@@ -29,59 +33,75 @@ clr.AddReference("RevitAPI")
 import Autodesk
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import *
-
+clr.AddReference("System.Core")
 clr.AddReference('System')
 clr.AddReference('RevitAPIUI')
 import pyrevit
-from pyrevit import forms
-
+from pyrevit import HOST_APP
+import System.Collections.Generic 
 import sys
 import string
 import random
+import System
+clr.ImportExtensions(System.Linq)
+
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 uiapp = __revit__
 app = uiapp.Application
 t = Autodesk.Revit.DB.Transaction(doc)
+a = Autodesk.Revit.DB.FilteredElementCollector(doc).OfClass(Autodesk.Revit.DB.ViewFamilyType)
+
+def get_viewtype(tr,a):
+    for vft in a.ToElements():
+        # Walkaround over an error while retrieving Name directly from ViewType
+        params = vft.Parameters
+        iterator = params.ForwardIterator()
+        iterator.Reset()
+
+        while iterator.MoveNext():
+            try:
+                if iterator.Current.Definition.Id.ToString() == "-1002001":
+                    if iterator.Current.AsString() == "Coordination Plan":
+                        return vft
+            except:
+                pass
+
+    try:
+        tr.Start("Create View Family Type")
+        old_v = get_viewtypePlan(a)
+        new_v = old_v.Duplicate("Coordination Plan")
+        try:
+            vft.DefaultTemplateId = 0
+        except:
+            pass
+        tr.Commit()
+        return new_v
+    except:
+        tr.RollBack()
 
 
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-def get_plan_viewtype():
-    collector = Autodesk.Revit.DB.FilteredElementCollector(doc).OfClass(Autodesk.Revit.DB.ViewFamilyType).ToElements()
-    print(collector)
-    for el in collector:
-        if el.ViewFamily == ViewFamily.FloorPlan:
-            viewFamTypeId = el.Id
-            return viewFamTypeId
+def get_viewtypePlan(elems):
+    for elem in elems:
+        if elem.FamilyName == "Floor Plan":
+            return elem
         else:
-            0
-
-def get_selected_elements():
-    """Return Selected Elements as a list[]. Returns empty list if no elements are selected.
-    Usage:
-    - Select 1 or more elements
-    > selected_elements = get_selected_elements()
-    > [<Autodesk.Revit.DB.FamilyInstance object at 0x0000000000000034 [Autodesk.Revit.DB.FamilyInstance]>]
-    """
-    selection = uidoc.Selection
-    selection_ids = selection.GetElementIds()
-    elements = []
-    for element_id in selection_ids:
-        elements.append(doc.GetElement(element_id))
-    return elements
+            return 0
 
 def get_categoryID(cat):
     if cat == "BasePoint":
-        collector = Autodesk.Revit.DB.Category.GetCategory(doc,BuiltInCategory.OST_ProjectBasePoint)
+        collector = Autodesk.Revit.DB.Category\
+            .GetCategory(doc,BuiltInCategory.OST_ProjectBasePoint)
     elif cat == "SurveyPoint":
-        collector = Autodesk.Revit.DB.Category.GetCategory(doc,BuiltInCategory.OST_SharedBasePoint)
+        collector = Autodesk.Revit.DB.Category\
+            .GetCategory(doc,BuiltInCategory.OST_SharedBasePoint)
     elif cat == "SitePoint":
-        collector = Autodesk.Revit.DB.Category.GetCategory(doc,BuiltInCategory.OST_SitePoint)
+        collector = Autodesk.Revit.DB.Category\
+            .GetCategory(doc,BuiltInCategory.OST_SitePoint)
     elif cat == "Site":
-        collector = Autodesk.Revit.DB.Category.GetCategory(doc,BuiltInCategory.OST_Site)          
+        collector = Autodesk.Revit.DB.Category\
+            .GetCategory(doc,BuiltInCategory.OST_Site)          
     else:
         pass
     return collector.Id
@@ -90,104 +110,69 @@ def make_active(a):
     uidoc.ActiveView = doc.GetElement(a.Id)
     pass
 
-class plan:
-    def __init__(self, Document):
-        self.doc = Document
+def find_coord(doc):
+    coord_views = []
+    elems = Autodesk.Revit.DB.FilteredElementCollector(doc).\
+                OfCategory(Autodesk.Revit.DB.BuiltInCategory.OST_Views).\
+                WhereElementIsNotElementType().ToElements()
+    for elem in elems:
+        if elem.ViewType == ViewType.FloorPlan and elem.IsTemplate == False :
+            if "Coordination Plan" in elem.Name:
+                coord_views.append(elem)
+    return coord_views
 
-    def create_plan(self):
-        newview = Autodesk.Revit.DB.ViewPlan.CreateAreaPlan(doc, areaSchemeId, levelId)
-        return newview
+def create_coord_plan(doc, vft, lvl):
+    elem = Autodesk.Revit.DB.ViewPlan.Create(doc, vft, lvl)
+    par = elem.get_Parameter(BuiltInParameter.VIEW_TEMPLATE)
+    if par != None:
+        par.Set(ElementId.InvalidElementId)
+    elem.DetailLevel = ViewDetailLevel.Coarse
+    elem.DisplayStyle = DisplayStyle.Wireframe
+    elem.Discipline = ViewDiscipline.Coordination
+    elem.Name = "Coordination Plan"
+    elem.SetCategoryHidden(get_categoryID("Site"), False)
+    elem.SetCategoryHidden(get_categoryID('BasePoint'), False)
+    elem.SetCategoryHidden(get_categoryID('SurveyPoint'), False)
+    #TODO Find out the category of Internal origin and turn it on
+    #(Autodesk.Revit.DB.InternalOrigin.IsHidden(elem))
+    #elem.SetCategoryHidden(get_categoryID('SitePoint'), False)
 
-    def duplicate_selected(self,view):
-        duplicate = view.Duplicate(ViewDuplicateOption.Duplicate)
-        return duplicate
+    #Change Crop View, Crop Region and Annotation Crop
+    elem.AreAnnotationCategoriesHidden = False
+    elem.CropBoxActive = False
+    elem.CropBoxVisible = False
+    ancrop = elem.get_Parameter(BuiltInParameter.VIEWER_ANNOTATION_CROP_ACTIVE)
+    ancrop.Set(0)
 
-    def check_selected(self,selection):
-        if selection.count == 1:
-            return True
-        else:
-            return False
-
-    def collect_links(self):
-        cl = Autodesk.Revit.DB.FilteredElementCollector(doc) \
-                .OfCategory(BuiltInCategory.OST_RvtLinks) \
-                .ToElementIds()
-        return cl
-
-    def adjust(self,view):
-        elem = doc.GetElement(view)
-        par = elem.get_Parameter(BuiltInParameter.VIEW_TEMPLATE)
-        if par != None:
-            par.Set(ElementId.InvalidElementId)
-        elem.DetailLevel = ViewDetailLevel.Coarse
-        elem.DisplayStyle = DisplayStyle.Wireframe
-        elem.Discipline = ViewDiscipline.Coordination
-        try:
-            elem.Name = "Coordination Plan"
-        except: 
-            pass
-        if elem.Name != "Coordination Plan":
-            try:
-                elem.Name = "Coordination Plan "  + id_generator()
-            except:
-                pyrevit.forms.alert("Naming Error")
-                t.RollBack()
-                sys.exit()
-        else:
-            pass
-        elem.SetCategoryHidden(get_categoryID("Site"), False)
-        elem.SetCategoryHidden(get_categoryID('BasePoint'), False)
-        elem.SetCategoryHidden(get_categoryID('SurveyPoint'), False)
-        #TODO Find out the category of Internal origin and turn it on
-        #(Autodesk.Revit.DB.InternalOrigin.IsHidden(elem))
-        #elem.SetCategoryHidden(get_categoryID('SitePoint'), False)
-        #TODO Change view Type (###_CoordPlan)
-
-        #Change Crop View, Crop Region and Annotation Crop
-        elem.AreAnnotationCategoriesHidden = False
-        elem.CropBoxActive = False
-        elem.CropBoxVisible = False
-        ancrop = elem.get_Parameter(BuiltInParameter.VIEWER_ANNOTATION_CROP_ACTIVE)
-        ancrop.Set(0)
-
-        #Change View range to unlimited
-        vr = elem.GetViewRange()
-        vr.SetLevelId(PlanViewPlane.TopClipPlane,PlanViewRange.Unlimited)
-        vr.SetLevelId(PlanViewPlane.BottomClipPlane,PlanViewRange.Unlimited)
-        vr.SetLevelId(PlanViewPlane.ViewDepthPlane,PlanViewRange.Unlimited)
-        elem.SetViewRange(vr)
-
-        return view
+    #Change View range to unlimited
+    vr = elem.GetViewRange()
+    vr.SetLevelId(PlanViewPlane.TopClipPlane,PlanViewRange.Unlimited)
+    vr.SetLevelId(PlanViewPlane.BottomClipPlane,PlanViewRange.Unlimited)
+    vr.SetLevelId(PlanViewPlane.ViewDepthPlane,PlanViewRange.Unlimited)
+    elem.SetViewRange(vr)
+    return elem
 
 def main():
-    if __shiftclick__:
-        pass
-    else :
-        selected = get_selected_elements()
-        if len(selected) == 1:
-            if selected[0].GetType().ToString() ==  'Autodesk.Revit.DB.ViewPlan' :
-                #print('WE HAVE A PLAN')
-                t.Start("Create Coordination Plan")
-                copied_plan = plan(doc).duplicate_selected(selected[0])
-                plan(doc).adjust(copied_plan)
-                t.Commit()
-                uidoc.ActiveView = doc.GetElement(copied_plan)
-                #TODO Implement Zoom To Fit or Focus on Base Point
-                #Autodesk.Revit.UI.UIView.ZoomToFit()
-                # for uv in uidoc.GetOpenUIViews():
-                #     if uv.ViewId.Equals(copied_plan):
-                #         uiview = uv
-                #         break
-                # uiview.ZoomToFit()
-                #uidoc.ShowElements("Base point <Element>")
-            else:
-                print('You need to select a plan view in Project Browser')
-        elif len(selected) >= 1:
-            pyrevit.forms.alert("You need to select only 1 plan in Project Browser")
-        elif len(selected) <= 1:
-            pyrevit.forms.alert("You need to select a plan view in Project Browser")
-        else:
-            pyrevit.forms.alert("Error")
-     
+    vf = get_viewtype(t,a)
+    level = Autodesk.Revit.DB.FilteredElementCollector(doc)\
+            .OfCategory(Autodesk.Revit.DB.BuiltInCategory.OST_Levels)\
+            .WhereElementIsNotElementType().ToElements().FirstOrDefault()
+
+    print(vf)
+    print(level.Id)
+    #Find existing coorfination plans
+    ex = find_coord(doc)
+    if len(ex) == 0:
+        t.Start("Create Coordination Plan")
+
+        new_plan = create_coord_plan(doc, vf.Id, level.Id)
+
+        t.Commit()
+        make_active(new_plan)
+    else:
+        #TODO: Implement options for what to do next
+        print("choose what to do next")
+
+
 if __name__ == '__main__':
     main()
