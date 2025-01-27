c2f7dff07176c80a3a07e51cb5823fe9dee4fecd
Roman Golev <67314258+romangolev@users.noreply.github.com>
2025-01-27 13:36:18 +0100

    feat: fix many join tool

--- pyArchitect.tab/Tools.panel/tools1.stack/JoinTool.splitbutton/ManyJoin1.pushbutton/script.py
+++ pyArchitect.tab/Tools.panel/tools1.stack/JoinTool.splitbutton/ManyJoin1.pushbutton/script.py
@@ -4,61 +4,35 @@
 
 #TODO: Implement the procedure of checking if elements are joined or not before executing command.
 
-__doc__ = """Joins multiple elements / Присоединяет множественные элементы 
-
-First you need to choose the category of elements to join\
-(script collects all elements of category). Then you need to choose \
-an element or elements to be joined to with the previous selected set of elements by category.
----------------------------------------------------------------------------
-Необходимо выбрать категорию элементов для множественного присоединения, \
-затем выбирается элемент или элементы к которым данное присоединение \
-необходимо применить. 
-"""
-__author__ = 'Roman Golev'
-__title__ = "Batch Join\nBy Category"
-
-import clr
-clr.AddReference('RevitAPI')
-import Autodesk
-from Autodesk.Revit.DB import *
-from Autodesk.Revit.UI.Selection import ObjectType
 import sys
-from core.catlistenum import get_catlist
+import Autodesk.Revit.DB as DB
 from pyrevit import forms
+from core.catlistenum import get_catlist
+from core.transaction import WrappedTransaction
 
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
-transaction = Autodesk.Revit.DB.Transaction(doc)
+uidoc = __revit__.ActiveUIDocument                 # type: ignore
+doc = __revit__.ActiveUIDocument.Document          # type: ignore
 
 def main():
-	catlist = get_catlist(doc)
-	ops = ['Columns', 'Walls', 'Floors', 'Roofs','Structural Columns', 'Structural Foundations', 'Structural Framing']
-	choice1 = forms.CommandSwitchWindow.show(ops, message='Select First Category to join')
-	try :
-		elements1 = catlist[choice1].WhereElementIsNotElementType().ToElements() 
-	except:
-		sys.exit()
-	ops.remove(choice1)
-	choice2 = forms.CommandSwitchWindow.show(ops, message='Select Option')
-	try :
-		elements2 = catlist[choice2].WhereElementIsNotElementType().ToElements() 
-	except:
-		sys.exit()
+    catlist = get_catlist(doc)
+    ops = ['Columns', 'Walls', 'Floors', 'Roofs','Structural Columns', 'Structural Foundations', 'Structural Framing']
+    choice1 = forms.CommandSwitchWindow.show(ops, message='Select First Category to join')
+    choice2 = forms.CommandSwitchWindow.show(ops, message='Select Second Category to join')
+    try :
+        elements1 = catlist[choice1].WhereElementIsNotElementType().ToElements()
+        elements2 = catlist[choice2].WhereElementIsNotElementType().ToElements() 
+    except:
+        sys.exit()
 
-
-	# TODO: Supress "Highlighted elements are joined but do not intersect." warning
-
-	results = []
-	transaction.Start('Multiple Join')
-	for A in elements1:
-		for B in elements2:
-			try:
-				result = Autodesk.Revit.DB.JoinGeometryUtils.JoinGeometry(doc,A,B)
-				results.append(result)
-			except:
-				pass
-
-	transaction.Commit()
+    results = []
+    with WrappedTransaction(doc, "Multiple Join", warning_suppressor=True):
+        for A in elements1:
+            for B in elements2:
+                try:
+                    result = DB.JoinGeometryUtils.JoinGeometry(doc, A, B)
+                    results.append(result)
+                except:
+                    pass
 
 if __name__ == '__main__':
     main()
--- pyArchitect.tab/Tools.panel/tools1.stack/JoinTool.splitbutton/ManyJoin2.pushbutton/script.py
+++ pyArchitect.tab/Tools.panel/tools1.stack/JoinTool.splitbutton/ManyJoin2.pushbutton/script.py
@@ -2,66 +2,44 @@
 # pylint: skip-file
 # by Roman Golev 
 
-__doc__ = """Joins multiple elements / Присоединяет множественные элементы 
-
-First you need to choose host element or elements to be joined to.\
-Then you need to choose a second group or element that should be connected to host.
---------------------------------------------------------------------------
-Необходимо выбрать первую группу элементов для множественного присоединения, \
-затем выбирается вторая группа элементов, к которым данное присоединение \
-необходимо применить. 
-"""
-__author__ = 'Roman Golev'
-__title__ = "Batch Join\nBy Selection"
-
-import clr
-clr.AddReference('RevitAPI')
-import Autodesk
-from Autodesk.Revit.DB import JoinGeometryUtils
-from Autodesk.Revit.UI.Selection import ObjectType, ISelectionFilter
-from Autodesk.Revit.Exceptions import OperationCanceledException
 import sys
+import Autodesk.Revit.DB as DB
+from Autodesk.Revit.UI.Selection import ObjectType, ISelectionFilter
 from core.selectionhelpers import CustomISelectionFilterByIdExclude, CustomISelectionFilterModelCats, ID_MODEL_ELEMENTS
+from core.transaction import WrappedTransaction
 
+uidoc = __revit__.ActiveUIDocument                      # type: ignore
+doc = __revit__.ActiveUIDocument.Document                # type: ignore
 
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
-transaction = Autodesk.Revit.DB.Transaction(doc)
+    #TODO: Exclude from second selection elements that are in first selection
 
 def main():
-	obj_type = ObjectType.Element
+    obj_type = ObjectType.Element
 
-	if __shiftclick__:
-		# Procedural filter for model elements
-		element_filter = CustomISelectionFilterModelCats(doc)
-	else:
-		# Non-Model Categories to hide
-		element_filter = CustomISelectionFilterByIdExclude(ID_MODEL_ELEMENTS)
-		
-	try:
-		selection1 = uidoc.Selection.PickObjects(obj_type, element_filter, "Selection Group 1")
-	except:
-		sys.exit()
-	try:
-		selection2 = uidoc.Selection.PickObjects(obj_type, "Selection Group 2")
-	except:
-		sys.exit()
+    if __shiftclick__:                                     # type: ignore
+        # Procedural filter for model elements
+        element_filter = CustomISelectionFilterModelCats(doc)
+    else:
+        # Non-Model Categories to hide
+        element_filter = CustomISelectionFilterByIdExclude(ID_MODEL_ELEMENTS)
+        
+    try:
+        selection1 = uidoc.Selection.PickObjects(obj_type, element_filter, "Selection Group 1")
+        selection2 = uidoc.Selection.PickObjects(obj_type, "Selection Group 2")
+    except:
+        sys.exit()
 
-	elementA = selection1
-	elementB = selection2
-
-	# TODO: Supress "Highlighted elements are joined but do not intersect." warning
-
-	results = []
-	transaction.Start('Multiple Join')
-	for A in elementA:
-		for B in elementB:
-			try:
-				join = JoinGeometryUtils.JoinGeometry(doc,doc.GetElement(A.ElementId),doc.GetElement(B.ElementId))
-			except Exception as error:
-				print(error)
-
-	transaction.Commit()
+    elementA = selection1
+    elementB = selection2
+    with WrappedTransaction(doc, "Multiple Join", warning_suppressor=True):
+        for A in elementA:
+            for B in elementB:
+                try:
+                    join = DB.JoinGeometryUtils.JoinGeometry(doc,
+                                        doc.GetElement(A.ElementId),
+                                        doc.GetElement(B.ElementId))
+                except Exception as error:
+                    print(error)
 
 if __name__ == '__main__':
     main()
